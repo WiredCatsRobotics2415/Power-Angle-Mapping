ae39afd74ec29dd4b24b5b435dd873e4dbe5c25b
Arxos0012 <jabaribooker@westminster.net>
2016-01-24 23:31:19 -0500

    Changes in which variable is monitored and how many times
    The velocity of the encoder for each side of the drivetrain is recorded
    10 times for each increment in CAN value instead of the change in angle
    for the IMU. The data has been recorded and analyzed to find the power
    to speed ratio of the Mule.

--- src/org/usfirst/frc/team2415/robot/Robot.java
+++ src/org/usfirst/frc/team2415/robot/Robot.java
@@ -1,13 +1,13 @@
 package org.usfirst.frc.team2415.robot;
 
 import java.io.BufferedWriter;
-
-import com.kauailabs.nav6.frc.IMU;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.SerialPort;
 
 public class Robot extends SampleRobot {
 	
@@ -16,7 +16,7 @@ public class Robot extends SampleRobot {
 	private final int[] LEFT_ENC = {0,1};
 	private final int[] RIGHT_ENC = {2,3};
 	
-	private final double POWER_INCREMEMT = 0.05;
+	private final double POWER_INCREMEMT = 0.01;
 	private BufferedWriter file;
 	
 	Transmission left, right;
@@ -24,13 +24,47 @@ public class Robot extends SampleRobot {
     public Robot(){
     	left = new Transmission(LEFT_TALS, LEFT_ENC);
     	right = new Transmission(RIGHT_TALS, RIGHT_ENC);
-    	
-    	WriteToFlashDrive.createBufferedWriter("Hermes", file);
+	
     }
     
-    public void robotInit() {}
+    public void writeToFile(double currentValue, double desiredValue) {
+		try {
+			file.write(currentValue + ",");
+			file.write(desiredValue + "\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+    
+    public void flushAndClose() {
+		try {
+    		System.out.println("I am about to flush the toilet");
+			file.flush();
+			file.close();
+			System.out.println("The dookie is down the drain");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+    
+    public void robotInit() {
+    	try {
+    		file = new BufferedWriter(new FileWriter(new File("/V/hermes.csv")));
+    	} catch (IOException e) {
+    		try {
+    			file = new BufferedWriter(new FileWriter(new File("/U/hermes.csv")));
+    		} catch (IOException f) {
+    			f.printStackTrace();
+    		}
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}
+    	
+    }
 
     public void autonomous() {
+    	
+    	
     	int numIntervals = (int)(2/POWER_INCREMEMT)+1;
     	double power;
     	for(int i=0; i<numIntervals; i++){
@@ -41,12 +75,15 @@ public class Robot extends SampleRobot {
     		Timer.start();
     		while(Timer.getTime() <= 0.5);
     		
-    		Timer.start();
-    		while(Timer.getTime() <= 1){
-    			WriteToFlashDrive.writeToFile(power, (left.getVel() + right.getVel())/2, file);
-    		}
+			for(int j = 0; j < 10; j++){
+    			Timer.start();
+    			while(Timer.getTime() < .1);
+    			writeToFile(power, (right.getVel()));
+			}
     	}
-    	WriteToFlashDrive.flushAndClose(file);
+    	flushAndClose();
+		left.setMotors(0);
+		right.setMotors(0);
     }
 
     public void operatorControl() {}
